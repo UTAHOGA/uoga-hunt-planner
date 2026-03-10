cfde9550274c4e59fb0981d79adf33dcb5370f5e
U.O.G.A. <tyler@uoga.org>
2026-03-10 07:45:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-fetch("./data/Utah_Hunt_Planner_Master_BuckDeer_Pages_43_46_48_51.json")
+// create map
+const map = L.map('map').setView([39.3, -111.7], 7);
+
+// basemap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+// LOAD HUNT DATA
+let huntData = [];
+
+fetch("./data/Utah_Hunt_Planner_Master_BuckDeer_Pages_43_53.json")
+.then(res => res.json())
+.then(data => {
+    huntData = data.records;
+    console.log("Loaded hunts:", huntData);
+});
+
+
