8756aa1dff29f97e87c522e6ca92deb576cb3262
U.O.G.A. <tyler@uoga.org>
2026-03-17 08:58:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const DWR_HUNT_BOUNDARY_LAYER = `${DWR_MAPSERVER}/0`;
 const DWR_HUNT_INFO_TABLE =
   'https://dwrmapserv.utah.gov/arcgis/rest/services/hunt/Boundaries_and_Tables/MapServer/1/query';
 const LOCAL_HUNT_BOUNDARIES_PATH = 'https://json.uoga.workers.dev/hunt-boundaries';
-const USFS_LOGO_URL = 'https://www.fs.usda.gov/sites/default/files/urbanforestry/FS-Insignia.png';
-const BLM_LOGO_URL = 'https://www.blm.gov/sites/blm.gov/files/styles/social_media/public/docs/2021-02/blm_09tri_color.png';
+const USFS_LOGO_URL = 'https://static.wixstatic.com/media/43f827_d54826fa897541e6810ba74cc2fcf9d7~mv2.png';
+const BLM_LOGO_URL = 'https://static.wixstatic.com/media/43f827_09ff64b7e1a34f88816d85f3384b2b40~mv2.png';
 
 const UNIT_CENTER_LOOKUP = {
   'beaver-east': [38.28, -112.48],
